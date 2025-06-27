32b7ab388fbeee4c92e117e1fd8548819548ce80
Parth1304 <97118975+Parth1304@users.noreply.github.com>
2025-06-27 15:38:56 +0530

    Testing if static files are being pushed

--- src/home/urls.py
+++ src/home/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_page_view
+from .views import about_view
 
 urlpatterns = [
     path("", home_page_view),
     path('hello-world/', home_page_view), 
+    path("about/", about_view),
     path('hello-world.html', home_page_view), 
     path('admin/', admin.site.urls),
 ]
