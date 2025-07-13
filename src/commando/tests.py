68cc9340c9802acb283f1157ee2ab1f385078675
Parth1304 <97118975+Parth1304@users.noreply.github.com>
2025-07-14 01:59:51 +0530

    4 - Test Django with Actions Secret Database URL

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+
+class RDSDBTestCase(TestCase):
+    def test_db_url(self):
+        DATABASE_URL = settings.DATABASE_URL
+        self.assertIn("rds.amazonaws.com", DATABASE_URL)
